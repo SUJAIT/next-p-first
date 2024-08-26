f850da6f6ebcdeba4f41b4e86d61d3923c72ce4f
SUJAIT <sujait2000@.com>
2024-08-26 22:06:46 +0600

    auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth/next";
+import  CredentialsProvider from 'next-auth/providers/credentials'
+
+const handler = NextAuth({
+    session: {
+        strategy : 'jwt'
+    },
+    providers : [
+
+        CredentialsProvider({
+            credentials : {
+                email : {label: "Email", type: "text", required:true,placeholder : "Your Email"},
+            password : {label : "Password", type: "password", required:true, placeholder: "your password" },
+           
+            },
+
+            async authorize (credentials){
+if(!credentials){
+return null;
+}
+return true;
+            }
+
+        })
+
+    ],
+});
+
+export { handler as GET, handler as POST};
